eee5a5c8b83739a5689536ed3d7881813dbb716e
leonardo060190 <leonardo_patricio@estudante.sesisenai.org.br>
2024-04-09 17:49:31 -0300

    ok

--- src/page/home/home.tsx
+++ src/page/home/home.tsx
@@ -1,7 +1,21 @@
+
+import { format } from "date-fns";
 import Header from "../../components/header";
+import { ptBR } from "date-fns/locale";
 
 const home = () => {
-  return <Header />;
+  return (
+  <div>
+  <Header />
+
+  <h2>Olá, Leonardo</h2>
+
+  <p className="capitalize">{format(new Date(), "EEEE',' dd 'de' MMMM", {
+    locale: ptBR,
+  } )}</p>
+
+  </div>
+)
 };
 
 export default home;
