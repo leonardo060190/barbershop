59818701f3545a3a09b1d1e026423cb706f86fbc
leonardo060190 <leonardo_patricio@estudante.sesisenai.org.br>
2024-06-05 17:10:02 -0300

    feito o set do login no localStorade

--- src/components/authProvider/AuthProvider.tsx
+++ src/components/authProvider/AuthProvider.tsx
@@ -1,4 +1,4 @@
-import React, { useState, createContext, useContext, ReactNode } from 'react';
+import React, { useState, createContext, useContext, ReactNode, useEffect } from "react";
 
 interface AuthContextType {
   autenticado: boolean;
@@ -12,7 +12,7 @@ const AuthContext = createContext<AuthContextType | null>(null);
 export const useAuth = (): AuthContextType => {
   const context = useContext(AuthContext);
   if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
+    throw new Error("useAuth must be used within an AuthProvider");
   }
   return context;
 };
@@ -22,7 +22,14 @@ interface AuthProviderProps {
 }
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
-  const [autenticado, setAutenticado] = useState<boolean>(false);
+  const [autenticado, setAutenticado] = useState<boolean>(() => {
+    const storeAuth = localStorage.getItem("autenticado");
+    return storeAuth ? JSON.parse(storeAuth) : false;
+  });
+
+  useEffect(()=>{
+    localStorage.setItem("autenticado", JSON.stringify(autenticado));
+  }, [autenticado]);
 
   const login = () => {
     setAutenticado(true);
